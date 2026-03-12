9a96621addfdee27eb73bfc3670cc33d3b1946ac
rokayaadel30-hub <rokayaadel30@gmail.com>
2026-03-12 09:24:36 +0200

    Update AI project.py

--- AI project.py
+++ AI project.py
@@ -1,7 +1,1 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
-data = pd.read_csv('student_exam_scores.csv')
+
